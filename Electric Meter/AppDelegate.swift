05a04f4927d54f27704226f185bc6d2c3476a20e
Zaw Min Thant <zawminthantt@gmail.com>
2016-06-11 00:20:20 +0800

    Added web service call and MBProgressHUD. Removed AlamofireNetworkActivityIndicator reference.

--- Electric Meter/AppDelegate.swift
+++ Electric Meter/AppDelegate.swift
@@ -7,7 +7,6 @@
 //
 
 import UIKit
-import AlamofireNetworkActivityIndicator
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -17,7 +16,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
-        NetworkActivityIndicatorManager.sharedManager.isEnabled = true
         
         return true
     }
--- Electric Meter/ViewController.swift
+++ Electric Meter/ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Alamofire
 
 class ViewController: UIViewController {
 
@@ -20,6 +21,41 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    override func viewWillAppear(animated: Bool) {
+        
+        let hud = MBProgressHUD.showHUDAddedTo(self.view, animated: true)
+        hud.label.text = "Loading ..."
+        
+        let url = NSURL(string: "http://eazydelivery.azurewebsites.net/pcm/svc/pcm/searchHistory/")!
+        let mutableURLRequest = NSMutableURLRequest(URL: url)
+        mutableURLRequest.HTTPMethod = "POST"
+        
+        let parameters = ["meterId" : "1",
+                          "locationId" : "1",
+                          "from" : "2016-06-05 23:59:05",
+                          "to" : "2016-06-08 23:59:05"]
+        
+        do {
+            mutableURLRequest.HTTPBody = try NSJSONSerialization.dataWithJSONObject(parameters, options: NSJSONWritingOptions())
+        } catch {
+            // No-op
+        }
+        
+        mutableURLRequest.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        
+        Alamofire.request(mutableURLRequest)
+            .validate()
+            .responseJSON { response in
+                if let JSON = response.result.value {
+                    dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), {
+                        print("JSON: \(JSON)")
+                        
+                        dispatch_async(dispatch_get_main_queue(), {
+                            MBProgressHUD.hideHUDForView(self.view, animated: true)
+                        })
+                    })
+                }
+        }
+    }
 }
 
